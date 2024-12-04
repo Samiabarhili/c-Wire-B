4eeaad40306d789c909e0e756a815c9a87910988
Synthia0401 <shrigaetry@gmail.com>
2024-12-04 10:45:02 +0100

    Update avl.h

--- codeC/avl.h
+++ codeC/avl.h
@@ -1,1 +1,6 @@
-//cc
+typedef struct AVL {
+    int valeur;                  // Valeur stockée dans le nœud
+    int hauteur;                 // Hauteur du nœud
+    struct AVL* gauche;          // Sous-arbre gauche
+    struct AVL* droite;          // Sous-arbre droit
+} AVL;
