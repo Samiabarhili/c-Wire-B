19f97b9f0d53bb368d3782170468a4e3ccb49bab
Hajarachour <hajarachour_78@hotmail.com>
2024-12-15 18:07:13 +0100

    Update avl_tree.h

--- codeC/avl_tree.h
+++ codeC/avl_tree.h
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 
-// Chaque   nœud   de   l’AVL   représente   une   station   et   va   donc   contenir
-// l’identifiant de la station ainsi que ses différentes données comme sa
-// capacité, ou bien la somme de ses consommateurs qui sera mise à jour
-// au fur et à mesure de la lecture des données par votre programme
+// Each node of the AVL represents a station and will therefore contain
+// the station identifier as well as its various data such as its
+// capacity, or the sum of its consumers which will be updated
+// as your program reads the data
 
 typedef struct AVLNode {
     int station_id;
@@ -29,6 +29,6 @@ AVLNode *DoubleRotateRight(AVLNode *node);
 AVLNode *insertAVL(AVLNode *node, int station_id, long capacity, long load, int *h);
 AVLNode *balanceAVL(AVLNode *node);
 AVLNode *freeAVL(AVLNode *node);
-void exportAVLNodeToFile(FILE *file, AVLNode *node);// Fonction pour parcourir l'arbre AVL et exporter les résultats dans un fichier
-void saveAVLNodeToFile(const char *filename, AVLNode *root);// Fonction pour sauvegarder l'arbre dans un fichier en commençant par ouvrir le fichier
+void exportAVLNodeToFile(FILE *file, AVLNode *node);// Function to traverse the AVL tree and export the results to a file
+void saveAVLNodeToFile(const char *filename, AVLNode *root);// Function to save the tree in a file starting by opening the file
 #endif //AVL_TREE_H
